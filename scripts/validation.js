7abb2bcd1fb83cb49fec5465a2de117673d34d42
James Ivy <ivyjamesjr@gmail.com>
2025-02-09 16:56:08 -0600

    update functons for toggleState

--- scripts/validation.js
+++ scripts/validation.js
@@ -11,11 +11,29 @@ const hideInputError = (formEl, inputEl) => {
 };
 
 const checkInputValidity = (formEl, inputEl) => {
-  console.log(inputEl.validationMessage);
   if (!inputEl.validity.valid) {
     showInputError(formEl, inputEl, inputEl.validationMessage);
   } else {
     hideInputError(formEl, inputEl);
+  }
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((input) => {
+    return !input.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonEl) => {
+  if (hasInvalidInput(inputList)) {
+    buttonEl.classList.add("button_inactive");
+    buttonEl.disabled = true;
+    buttonEl.classList.remove("modal__save-button_disabled");
+    //Don't forget the CSS
+  } else {
+    buttonEl.disabled = false;
+    buttonEl.classList.remove("button_inactive");
+    buttonEl.classList.add("modal__submit-btn_disabled");
   }
 };
 
@@ -29,7 +47,7 @@ const setEventListeners = (formEl) => {
   inputList.forEach((inputElement) => {
     inputElement.addEventListener("input", function () {
       checkInputValidity(formEl, inputElement);
-      //toggleButtonState(inputList, buttonElement);
+      toggleButtonState(inputList, buttonElement);
     });
   });
 };
